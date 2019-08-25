c40766122fd33c114a094ea082547f5b2171140b
yhu <whoyoung@gatech.edu>
2019-08-24 20:29:54 -0400

    css styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,7 +4,7 @@ import "./Pokecard.css";
 const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 // "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-let padToThree = (number) => (number <= 1000 ? `00${number}`.slice(-3) : number)
+let padToThree = number => (number <= 1000 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render() {
@@ -12,7 +12,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
